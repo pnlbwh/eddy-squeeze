646b596d8ea16d782a205cbc9502ff88c3231c64
kevin cho <sky8671@gmail.com>
2020-11-26 13:17:34 -0500

    feat: argparse option test with bash script

---
+++ tests/bin/test_eddy_squeeze.sh
@@ -0,0 +1,28 @@
+python ../../bin/eddy_squeeze -h
+
+python ../../bin/eddy_squeeze \
+    --eddy_directories \
+        ../prac_study_dir/subject01 \
+        ../prac_study_dir/subject02 \
+        ../prac_study_dir/subject03 \
+    --out_dir prac_eddy_summary \
+    --print_table
+
+python ../../bin/eddy_squeeze \
+    --eddy_directories \
+        ../prac_study_dir/subject01 \
+        ../prac_study_dir/subject02 \
+        ../prac_study_dir/subject03 \
+    --out_dir prac_eddy_summary \
+    --save_html
+
+
+python ../../bin/eddy_squeeze \
+    --eddy_directories \
+        ../prac_study_dir/subject01 \
+        ../prac_study_dir/subject02 \
+        ../prac_study_dir/subject03 \
+    --out_dir prac_eddy_summary \
+    --figure \
+    --save_html
+
