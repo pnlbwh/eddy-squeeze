5aa908b9cc0b5d46c6ba9e013597a204cb6b0862
kcho <kevincho@bwh.harvard.edu>
2022-03-16 15:19:24 -0400

    feat: save_all_outlier_slices_in_detail added

--- tests/eddy_squeeze_lib/test_eddy_present.py
+++ tests/eddy_squeeze_lib/test_eddy_present.py
@@ -6,7 +6,7 @@ import pytest
 
 script_loc = Path(os.path.realpath(__file__))
 script_dir = script_loc.parent
-test_root_dir = script_dir.parent
+test_root_dir = script_dir.parent.parent / 'test'
 root_dir = test_root_dir.parent
 print(root_dir)
 print(test_root_dir)
@@ -30,7 +30,11 @@ eddy_postfixes = ['post_eddy_shell_alignment_parameters',
 
 @pytest.fixture
 def eddyRun():
-    eddy_prefix = test_root_dir / 'prac_out' / 'subject01-eddy_out'
+
+    # data_dir = Path('/data/pnl/Collaborators/Shanghai_Prodrome')
+    # test_root_dir = data_dir / 'all_data_preprocessed' / 'Ndyx6030' / 'diff'
+    # eddy_prefix = test_root_dir / 'prac_out' / 'subject01-eddy_out'
+    eddy_prefix = test_root_dir / 'eddy_out' / '197-dwi_eddy_out_repol'
     eddyRun = EddyRun(eddy_prefix)
     eddyRun.read_file_locations_from_command()
     eddyRun.load_eddy_information()
@@ -71,5 +75,48 @@ def test_plot_pre_post_correction_slice(eddyRun):
             std, sqr_std, eddyRun.outlier_std_array,
             eddyRun.restricted_movement_array)
         break
+
+    assert Path(outfile).is_file()
+
+
+def test_plot_pre_post_correction_slice_detail(eddyRun):
+    eddyRun.load_data()
+    # plot them
+    for v_init, s, std, sqr_std, r in zip(eddyRun.outlier_vol,
+                                          eddyRun.outlier_slice,
+                                          eddyRun.stds,
+                                          eddyRun.sqr_stds,
+                                          eddyRun.rank):
+        bvalue = eddyRun.bvalue_arr[v_init]
+        bvalue_volumes = np.where(eddyRun.bvalue_arr == bvalue)[0]
+        print(v_init)
+        print(bvalue)
+        print(bvalue_volumes)
+        same_slice_vol_num = np.where(eddyRun.outlier_slice == s)
+        same_slice_outlier_vols = eddyRun.outlier_vol[same_slice_vol_num]
+        print(same_slice_outlier_vols)
+        for v in bvalue_volumes:
+            pre_data_tmp = eddyRun.pre_data[:, :, s, v]
+            pre_data_tmp = np.where(eddyRun.mask_data[:, :, s] == 1,
+                                    pre_data_tmp, 0)
+            post_data_tmp = eddyRun.post_data[:, :, s, v]
+            post_data_tmp = np.where(eddyRun.mask_data[:, :, s] == 1,
+                                     post_data_tmp, 0)
+
+            sagittal_data = eddyRun.pre_data[eddyRun.mid_point, :, :, v].T
+            sagittal_data_fixed = eddyRun.post_data[eddyRun.mid_point, :, :, v].T
+
+            outfile = f'shell_{int(bvalue)}_slice_{s:02}_prac_{r:03}_vol_{v}.png'
+
+            plot_pre_post_correction_slice(
+                eddyRun.eddy_dir,
+                pre_data_tmp, post_data_tmp,
+                sagittal_data, sagittal_data_fixed,
+                outfile,
+                s, v, bvalue, r,
+                std, sqr_std, eddyRun.outlier_std_array,
+                eddyRun.restricted_movement_array,
+                outlier_vols=same_slice_outlier_vols)
+        return
 
     assert Path(outfile).is_file()
